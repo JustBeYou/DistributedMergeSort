039e24086f2a9958acf35df5085839ea2a7b809e
JustBeYou <mihailferaru2000@gmail.com>
2019-06-09 18:56:07 +0300

    Implemented classic merge sort

--- main.go
+++ main.go
@@ -19,12 +19,61 @@ func randomVector(size, max int64) []int64 {
 	return ret
 }
 
+// ascending order
+func clasicMergeSort(vec []int64) []int64 {
+	sorted := make([]int64, len(vec))
+	
+	if len(vec) == 1 {
+		sorted[0] = vec[0]
+	} else if len(vec) == 2 {
+		if vec[0] < vec[1] {
+			sorted[0], sorted[1] = vec[0], vec[1]
+		} else {
+			sorted[0], sorted[1] = vec[1], vec[0]
+		}
+	} else {
+		A := clasicMergeSort(vec[:len(vec)/2])
+		B := clasicMergeSort(vec[len(vec)/2:])
+		i, j, idx := 0, 0, 0
+
+		for i < len(A) && j < len(B) {
+			if A[i] < B[j] {
+				sorted[idx] = A[i]
+				idx++
+				i++
+			} else if A[i] > B[j]{
+				sorted[idx] = B[j]
+				idx++
+				j++
+			} else {
+				sorted[idx], sorted[idx + 1] = A[i], B[j]
+				i++
+				j++
+			}
+		}
+
+		for i < len(A) {
+			sorted[idx] = A[i]
+			i++
+			idx++
+		}
+		
+		for j < len(B) {
+			sorted[idx] = B[j]
+			j++
+			idx++
+		}
+	}
+
+	return sorted
+}
+
 func main() {
 	fmt.Println("Parallel implementation of merge sort algorithm")
 
-	v := randomVector(100, 1000)
-	for _, val := range v {
-		fmt.Printf("%d ", val)
-	}
-	fmt.Printf("\n")
+	v := randomVector(10, 1000)
+	fmt.Println(v)
+	sorted_v := clasicMergeSort(v)
+	fmt.Println(sorted_v)
+
 }
